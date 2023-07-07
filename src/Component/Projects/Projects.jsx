2c2a5a51b66f015bf2b41fafb9ef9562356280aa
mdasik0 <asikthe1st@gmail.com>
2023-07-07 12:22:13 +0600

    all done

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -2,7 +2,7 @@
 const Projects = () => {
     return (
         <section id="projects" className="lg:h-screen">
-            <h3>Hello time to do some projects</h3>
+            <h3>Hello some projects</h3>
         </section>
     );
 };
