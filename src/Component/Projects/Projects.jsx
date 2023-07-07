4cdd53047d35c826557f860d0ade3bade03c5301
mdasik0 <asikthe1st@gmail.com>
2023-07-07 12:12:04 +0600

    due to some problem i am making my first commit in the middle of the project

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -2,7 +2,7 @@
 const Projects = () => {
     return (
         <section id="projects" className="lg:h-screen">
-            <h3>Projects</h3>
+            <h3>Hello time to do some projects</h3>
         </section>
     );
 };
